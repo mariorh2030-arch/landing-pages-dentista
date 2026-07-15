3a42304a438241dff8ff71dfd5ffba1dc52ea6aa
crostyty <mariorosalez2020@gmail.com>
2026-07-14 21:00:48 -0600

    Se arregla el error que existia al insertar los datos desde el formulario

--- Backend/SRC/Models/paciente.model.js
+++ Backend/SRC/Models/paciente.model.js
@@ -4,9 +4,11 @@ const obtenerPacientes = async () => {
     const [rows] = await pool.query("SELECT * FROM pacientes");
     return rows;
 }
-const insertarPacientes = async (nombre, apellidos, telefono, corre, fechaNacimiento) =>{
-    const [rows] = await pool.query("INSERT INTO pacientes(nombre, apellidos, telefono, correo, fechaNacimiento) values(?, ?, ?, ?, ?)"), 
-    [nombre, apellidos, telefono, corre, fechaNacimiento];
+const insertarPacientes = async (nombre, apellidos, telefono, correo, fechaNacimiento) =>{
+    const [rows] = await pool.query(`INSERT INTO pacientes
+        (nombre, apellidos, telefono, correo, fechaNacimiento)
+        VALUES (?, ?, ?, ?, ?)`,
+        [nombre, apellidos, telefono, correo, fechaNacimiento]);
     return rows;
     
 }
